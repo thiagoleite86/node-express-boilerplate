e8dcca20ce02efdeacd010a8f2921546fe8d1da2
thiagoleite86 <thiago.leite@nad1.com.br>
2016-02-04 17:20:34 -0300

    Added body-parser

--- server.js
+++ server.js
@@ -4,7 +4,14 @@
 * Module dependencies
 */
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+/*
+* Configure App
+*/
+app.use(bodyParser.urencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/teste', function(req, res) {
 	res.send('hello world');
